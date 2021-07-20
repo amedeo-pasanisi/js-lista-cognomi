2e00851a9b82eda176447983e69f3adf204949e7
amedeo <amedeopasanisi97@gmail.com>
2021-07-20 14:43:20 +0200

    stampa la lista ordinata

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 // 1. chiedi all’utente il cognome
+var cognome = prompt("inserisci il tuo cognome");
+
+// do {
+// } while ();
+
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var lista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+console.log("lista iniziale: " + lista);
+
+lista.push(cognome.charAt(0).toUpperCase() + cognome.slice(1));
+console.log("lista aggiornata: " + lista);
+
 // 3. stampa la lista ordinata alfabeticamente
+lista.sort();
+
+for (var i = 0; i < lista.length; i++) {
+    console.log(lista[i]);
+}
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
