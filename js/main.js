81463028567735439dd75b37735bcf3b1971f558
amedeo <amedeopasanisi97@gmail.com>
2021-07-20 15:09:21 +0200

    correzione fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 // 1. chiedi all’utente il cognome
 var cognome = prompt("inserisci il tuo cognome");
 
-// do {
-// } while ();
-
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 var lista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 console.log("lista iniziale: " + lista);
 
-lista.push(cognome.charAt(0).toUpperCase() + cognome.slice(1));
+var cognomeMaiuscolo = cognome.charAt(0).toUpperCase() + cognome.slice(1);
+
+lista.push(cognomeMaiuscolo);
 console.log("lista aggiornata: " + lista);
 
 // 3. stampa la lista ordinata alfabeticamente
@@ -19,8 +18,11 @@ for (var i = 0; i < lista.length; i++) {
     console.log(lista[i]);
 }
 
-// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-console.log("lista ordinata alfabeticamente numerata:");
+console.log("lista ordinata alfabeticamente e numerata:");
 for (var i = 0; i < lista.length; i++) {
     console.log((i + 1) + " " + lista[i]);
 }
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+console.log("posizione nella lista dell' ultimo cognome aggiunto: " + (lista.indexOf(cognomeMaiuscolo) + 1));
