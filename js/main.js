f064faa9861ad7adcb4a47d593beca7fd21bd389
amedeo <amedeopasanisi97@gmail.com>
2021-07-20 15:18:24 +0200

    riordine del codice

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 // 1. chiedi all’utente il cognome
 var cognome = prompt("inserisci il tuo cognome");
+var cognomeMaiuscolo = cognome.charAt(0).toUpperCase() + cognome.slice(1);
 
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 var lista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 console.log("lista iniziale: " + lista);
-
-var cognomeMaiuscolo = cognome.charAt(0).toUpperCase() + cognome.slice(1);
 
 lista.push(cognomeMaiuscolo);
 console.log("lista aggiornata: " + lista);
@@ -24,5 +23,4 @@ for (var i = 0; i < lista.length; i++) {
 }
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-
 console.log("posizione nella lista dell' ultimo cognome aggiunto: " + (lista.indexOf(cognomeMaiuscolo) + 1));
